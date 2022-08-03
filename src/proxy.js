6264b22eed616c3048ff57beee7cd1eefd4979a2
Marcel Lindig <ml@nullpilot.de>
2022-08-03 15:46:12 +0200

    feat: handle work_validate requests

--- src/proxy.js
+++ src/proxy.js
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import got from 'got';
+import blake2 from 'blake2';
 
 let serviceId;
 let apiKey;
@@ -69,6 +70,45 @@ async function forwardRequest(data, reply) {
   }
 }
 
-function handleWorkValidate(data) {
+function handleWorkValidate(data, reply) {
+  const baseDifficulty =    BigInt("0xfffffff800000000")
+  const receiveDifficulty = BigInt("0xfffffe0000000000")
+  const hash = Buffer.from(data.hash, 'hex')
+  const work = Buffer.from(data.work, 'hex')
+  const difficulty = workDifficulty(hash, work)
+  const multiplier = workMultiplier(difficulty, baseDifficulty)
+  const payload = {
+    valid_receive: isWorkValid(difficulty, receiveDifficulty),
+    valid_all: isWorkValid(difficulty, baseDifficulty),
+    multiplier: multiplier.toString(),
+    difficulty: difficulty.toString('hex'),
+  }
 
+  if(data.difficulty) {
+    payload.valid = isWorkValid(difficulty, BigInt(`0x${workdata.difficulty}`))
+  }
+
+  reply.send(payload)
+}
+
+function workDifficulty(hash, work) {
+  return blake2.createHash('blake2b', {digestLength: 8})
+  .update(work.reverse())
+  .update(hash)
+  .digest()
+  .reverse()
+  .toString('hex')
+}
+
+function workMultiplier(workDifficulty, baseDifficulty) {
+  const exp = 0x10000000000000000n;
+  const diffn = BigInt(`0x${workDifficulty}`);
+  const eb = Number(exp - baseDifficulty)
+  const ed = Number(exp - diffn)
+  return eb / ed;
+}
+
+function isWorkValid(workDifficulty, baseDifficulty) {
+  const diffn = BigInt(`0x${workDifficulty}`);
+  return (diffn > baseDifficulty) ? "1" : "0";
 }
